926e7cb7979d97f28d71db8cb5e6ea7b504e8a66
bspoloo <animetx098@gmail.com>
2024-11-02 15:10:24 -0400

    Added all controller to the entities

---
+++ TeachWaveAPI.Infraestructure/Configuration/ConfigScoped.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeachWaveAPI.Application.Interfaces;
+using TeachWaveAPI.Application.Services;
+using TeachWaveAPI.Core.Interfaces;
+using TeachWaveAPI.Infraestructure.Persistence.Repositories;
+
+namespace TeachWaveAPI.Infraestructure.Configuration
+{
+    public class ConfigScoped
+    {
+        public static void AddScopedServices(IServiceCollection service)
+        {
+            service.AddScoped<IPersonService, PersonService>();
+            service.AddScoped<IPersonRepository, PersonRepository>();
+
+            service.AddScoped<IUserService, UserService>();
+            service.AddScoped<IUserRepository, UserRepository>();
+
+            service.AddScoped<ICourseService, CourseService>();
+            service.AddScoped<ICourseRepository, CourseRepository>();
+
+            service.AddScoped<IModuleService, ModuleService>();
+            service.AddScoped<IModuleRepository, ModuleRepository>();
+
+            service.AddScoped<IEnrollmentService, EnrollmentService>();
+            service.AddScoped<IEnrollmentRepository, EnrollmentRepository>();
+
+            service.AddScoped<IAssignmentService, AssignmentService>();
+            service.AddScoped<IAssignmentRepository, AssigmentRepository>();
+
+            service.AddScoped<ISubmissionService, SubmissionService>();
+            service.AddScoped<ISubmissionRepository, SubmissionRepositoy>();
+        }
+    }
+}
--- TeachWaveAPI.Presentation/Program.cs
+++ TeachWaveAPI.Presentation/Program.cs
@@ -1,11 +1,8 @@
 using AutoMapper;
 using Microsoft.EntityFrameworkCore;
-using TeachWaveAPI.Application.Interfaces;
 using TeachWaveAPI.Application.Mappings;
-using TeachWaveAPI.Application.Services;
-using TeachWaveAPI.Core.Interfaces;
+using TeachWaveAPI.Infraestructure.Configuration;
 using TeachWaveAPI.Infraestructure.Persistence.Contexts;
-using TeachWaveAPI.Infraestructure.Persistence.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -31,26 +28,7 @@ IMapper mapper = mapperConfig.CreateMapper();
 
 builder.Services.AddSingleton(mapper);
 
-builder.Services.AddScoped<IPersonService, PersonService>();
-builder.Services.AddScoped<IPersonRepository, PersonRepository>();
-
-builder.Services.AddScoped<IUserService, UserService>();
-builder.Services.AddScoped<IUserRepository, UserRepository>();
-
-builder.Services.AddScoped<ICourseService, CourseService>();
-builder.Services.AddScoped<ICourseRepository, CourseRepository>();
-
-builder.Services.AddScoped<IModuleService, ModuleService>();
-builder.Services.AddScoped<IModuleRepository, ModuleRepository>();
-
-builder.Services.AddScoped<IEnrollmentService, EnrollmentService>();
-builder.Services.AddScoped<IEnrollmentRepository, EnrollmentRepository>();
-
-builder.Services.AddScoped<IAssignmentService, AssignmentService>();
-builder.Services.AddScoped<IAssignmentRepository, AssigmentRepository>();
-
-builder.Services.AddScoped<ISubmissionService, SubmissionService>();
-builder.Services.AddScoped<ISubmissionRepository, SubmissionRepositoy>();
+ConfigScoped.AddScopedServices(builder.Services);
 
 var app = builder.Build();
 
